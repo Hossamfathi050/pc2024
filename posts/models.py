b66acf491358d1f99e5ceda255781cdbbf19ab53
unknown <hossamfathi050@outlook.com>
2024-03-29 01:48:26 +0300

    first commit

--- posts/models.py
+++ posts/models.py
@@ -5,13 +5,14 @@ from django.db import models
 
 
 class Post(models.Model):
-    post_title = models.CharField(max_length=200)
+    post_title = models.CharField(max_length=150)
+    post_desc= models.CharField(max_length=450)
     post_date = models.DateTimeField("date published")
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
 
 
