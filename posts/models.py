a5860e774fa73cd13af032ed9432c77c4d7d8f6f
unknown <hossamfathi050@outlook.com>
2024-03-29 01:45:18 +0300

    first commit

---
+++ posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=200)
+    post_date = models.DateTimeField("date published")
+
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+
+
