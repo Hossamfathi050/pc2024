5ab5b5e8288d977bdee5a436cb7e6a5c11cdf485
unknown <hossamfathi050@outlook.com>
2024-03-29 01:46:57 +0300

    first commit

--- posts/models.py
+++ posts/models.py
@@ -9,9 +9,9 @@ class Post(models.Model):
     post_date = models.DateTimeField("date published")
 
 
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
 
